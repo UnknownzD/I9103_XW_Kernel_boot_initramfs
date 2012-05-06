675ea28632dd0eb2642e3084a904d199dde95b43
UnknownzD <UnknownzD@gmail.com>
2012-05-06 19:43:04 +0800

    Post init script update part 3 - Extract all su file and other things automatically

--- sbin/post-init.sh
+++ sbin/post-init.sh
@@ -29,22 +29,22 @@ copy_file ()
 			write_file=1;
 		fi
 	elif [ $3 -eq 1 ]; then
-		if[ ! -f $2 ]; then
+		if [ ! -f $2 ]; then
 			write_file=1;
 		fi
 	elif [ $3 -eq 2 ]; then
-		if [ ! -h $2 ]; then
+		if !([ -h $2 ] && [ $(eval readlink $2) == '$1' ]); then
 			write_file=2;
 		fi
 	fi
 	if [ $write_file -eq 1 ]; then
 		del_file $2
-		$busybox cp -f -$1 $2 >/dev/null 2>&1
+		$busybox cp -f $1 $2 >/dev/null 2>&1
 		$busybox chown $5 $2 >/dev/null 2>&1
 		$busybox chmod $4 $2 >/dev/null 2>&1
 	elif [ $write_file -eq 2 ]; then
 		del_file $2
-		$busybox ln -s $1 $2 >/dev/null 2>&1
+		$busybox ln -f -s $1 $2 >/dev/null 2>&1
 	fi
 }
 
@@ -53,16 +53,23 @@ $busybox mount -o remount,rw /system
 ##### Install SU #####
 copy_file /sbin/su /system/bin/su 0 4755 0:2000
 copy_file /sbin/Superuser.apk /system/app/Superuser.apk 1 644 0:0
-copy_file /system/bin/su /system/xbin/su 3
+copy_file /system/bin/su /system/xbin/su 2
 
 ##### Install busybox #####
-copy_file /sbin/busybox /system/xbin/busybox 0 755 0:0
+copy_file /sbin/busybox /system/bin/busybox 0 755 0:0
+cd /sbin/
+for file in ./*; do
+	if [ -h $file ] && [ $(eval readlink $file) == 'busybox' ]; then 
+		copy_file busybox /system/bin/$file 2
+	fi
+done
+	
 
 ##### /system/etc/init.d tweak (run custom scripts) #####
-if [-d /system/etc/init.d]; then
+if [ -d /system/etc/init.d ]; then
     for file in /system/etc/init.d/* ; do
-	if [-f $file]; then
-		/system/bin/sh "$file"
+	if [ -f $file ]; then
+		/system/bin/sh "$file" >/dev/null 2>&1
 	fi
     done
 fi
