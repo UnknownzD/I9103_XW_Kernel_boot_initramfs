64d1d70c3631e6d900812436e2f501ee3151ff34
UnknownzD <UnknownzD@gmail.com>
2012-05-09 16:17:44 +0800

    Reverted to d59b3fd4b4 with patches

--- sbin/post-init.sh
+++ sbin/post-init.sh
@@ -56,94 +56,6 @@ copy_file ()
 $busybox mount -o remount,rw /system
 $busybox mount -o remount,rw /data
 
-# Remount each file system with noatime and nodiratime flags to save battery and CPU cycles
-for k in $(mount | grep relatetime | cut -d " " -f3)
-do
-	$busybox sync;
-	$busybox mount -o remount,noatime,norelatime,nodiratime $k;
-done;
-
-for k in $(mount | grep ext4 | cut -d " " -f1)
-do
-	$busybox sync;
-	$busybox mount -o remount,ro,async,noatime,norelatime,nodiratime,noauto_da_alloc,delalloc,barrier=0,errors=remount-ro,data=writeback,nobh $k;
-	$busybox sync;
-	/sbin/tune2fs -f -o journal_data_writeback -O ^has_journal $k;
-	$busybox sync;
-	$busybox mount -o remount,rw,async,noatime,norelatime,nodiratime,noauto_da_alloc,delalloc,barrier=0,errors=remount-ro,data=writeback,nobh $k;
-done;
-
-for k in $(mount | grep vfat | cut -d " " -f3)
-do
-	$busybox sync;
-	$busybox mount -o remount,async,noatime,norelatime,nodiratime,errors=remount-ro $k;
-done;
-
-# Select sio as default IO scheduler
-# Only the first 4 has I/O scheduler
-STL=`ls -d /sys/block/stl*`;
-BML=`ls -d /sys/block/bml*`;
-MMC=`ls -d /sys/block/mmc*`;
-TFSR=`ls -d /sys/block/tfsr*`;
-# Belows are without I/O scheduler, be careful to mess with them!
-ZRM=`ls -d /sys/block/zram*`;
-RAM=`ls -d /sys/block/ram*`;
-LOOP=`ls -d /sys/block/loop*`;
-# Optimize non-rotating storage; 
-for i in $MMC $LOOP
-do
-	# Select sio I/O scheduler as default
-	if [ -e $i/queue/scheduler ];
-	then
-		$busybox sync;
-		$busybox echo "sio" > $i/queue/scheduler;
-	fi;
-	if [ -e $i/queue/rotational ]; 
-	then
-		$busybox sync;
-		$busybox echo "0" > $i/queue/rotational; 
-	fi;
-	if [ -e $i/queue/nr_requests ];
-	then
-		$busybox sync;
-		$busybox echo "1024" > $i/queue/nr_requests; # for starters: keep it sane
-	fi;
-	if [ -e $i/queue/rq_affinity ];
-	then
-		$busybox sync;
-		$busybox echo "1"   >  $i/queue/rq_affinity;
-	fi;
-	if [ -e $i/queue/read_ahead_kb ];
-	then
-		$busybox sync;
-		$busybox echo "2048" >  $i/queue/read_ahead_kb;
-	fi;
-	if [ -e $i/queue/iostats ];
-	then
-		$busybox sync;
-		$busybox echo "0" > $i/queue/iostats;
-	fi;
-	# Below is SIO specific configuration
-	if [ -e $i/queue/iosched/async_expire ];
-	then
-		$busybox sync;
-		$busybox echo "1000" > $i/queue/iosched/async_expire ];
-	fi;
-	if [ -e $i/queue/iosched/fifo_batch ];
-	then
-		$busybox sync;
-		$busybox echo "1" > $i/queue/iosched/fifo_batch;
-	fi;
-	if [ -e $i/queue/iosched/sync_expire ];
-	then
-		$busybox sync;
-		$busybox echo "500" > $i/queue/iosched/sync_expire ];
-	fi;
-done;
-
-# Increase the read ahead size
-for file in $(ls -d /sys/devices/virtual/bdi/*/read_ahead_kb); do $busybox echo "2048" > $file; done
-
 ##### Remove dalvik-cache and cache #####
 $busybox rm -rf /data/dalvik-cache/*
 $busybox rm -rf /data/cache/*
@@ -154,14 +66,12 @@ copy_file /sbin/su /system/bin/su 0 6755 0:0
 copy_file /tmp/Superuser.apk /system/app/Superuser.apk 1 644 0:0
 copy_file /system/bin/su /system/xbin/su 2
 
-##### Install busybox and other binaries #####
+##### Install busybox #####
 copy_file /sbin/busybox /system/bin/busybox 0 755 0:0
 copy_file /sbin/e2fsck /system/bin/e2fsck 0 755 0:0
 copy_file /sbin/mke2fs /system/bin/mke2fs 0 755 0:0
 copy_file /sbin/parted /system/bin/parted 0 755 0:0
-copy_file /sbin/sqlite3 /system/bin/sqlite3 0 755 0:0
 copy_file /sbin/tune2fs /system/bin/tune2fs 0 755 0:0
-copy_file /sbin/zipalign /system/bin/zipalign 0 755 0:0
 cd /sbin/
 for file in ./*; do
 	if [ "$(eval readlink $file)" == 'busybox' ]; then 
@@ -183,58 +93,14 @@ copy_file /tmp/be_photo /system/etc/be_photo 1 755 0:0
 copy_file /tmp/com.sonyericsson.suquashi.xml /system/etc/permissions/com.sonyericsson.suquashi.xml 1 644 0:0
 copy_file /tmp/libswiqibmpcnv.so /system/lib/libswiqibmpcnv.so 1 644 0:0
 
-##### Load sysctl configuration #####
+##### Load configuration #####
 sysctl -p /sysctl.conf
-
-##### sqlite3 db optimization #####
-if [ -d "/data" ]; then
-	mount -o remount,rw /data;
-	for i in "$busybox find /data -iname "*.db""; 
-	do \
-		/sbin/sqlite3 $i 'VACUUM;'; 
-		/sbin/sqlite3 $i 'REINDEX;'; 
-	done;
-fi;
-
-if [ -d "/system" ]; then
-	mount -o remount,rw /system;
-	for i in "$busybox find /system -iname "*.db""; 
-	do \
-		/sbin/sqlite3 $i 'VACUUM;'; 
-		/sbin/sqlite3 $i 'REINDEX;'; 
-	done;
-	mount -o remount,ro /system;
-fi;
-
-if [ -d "/mnt/sdcard" ]; then
-	mount -o remount,rw /sdcard;
-	for i in "$busybox find /mnt/sdcard -iname "*.db""; 
-	do \
-		/sbin/sqlite3 $i 'VACUUM;'; 
-		/sbin/sqlite3 $i 'REINDEX;'; 
-	done;
-fi;
-
-# Disable carrieriq service
-/system/bin/pm disable android/com.carrieriq.iqagent.service.IQService
-/system/bin/pm disable android/com.carrieriq.iqagent.service.receivers.BootCompletedReceiver
-/system/bin/pm disable android/com.carrieriq.iqagent.service.ui.DebugSettings
-/system/bin/pm disable android/com.carrieriq.iqagent.service.ui.ShowMessage
-/system/bin/pm disable android/com.carrieriq.iqagent.client.NativeClient
-/system/bin/pm disable android/com.carrieriq.iqagent.stdmetrics.survey.android.QuestionnaireLaunchActivity
-/system/bin/pm disable android/com.carrieriq.iqagent.stdmetrics.survey.android.QuestionnaireActivity
-
-# Disable user stat and data collection
-$busybox chmod 000 /data/system/userbehavior.db;
-$busybox chmod 000 /data/system/usagestats/;
-$busybox chmod 000 /data/system/appusagestats/;
-
 
 ##### /system/etc/init.d tweak (run custom scripts) #####
 if [ -d '/system/etc/init.d' ]; then
     for file in /system/etc/init.d/* ; do
 	if [ -f "$file" ]; then
-		/sbin/sh "$file" >/dev/null 2>&1
+		/system/bin/sh "$file" >/dev/null 2>&1
 	fi
     done
 fi
